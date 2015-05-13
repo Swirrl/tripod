f4f36b591b455d19d3ceee27a22e6eb4abf07f1e
Robin Gower <robin.git@infonomics.ltd.uk>
2015-05-13 18:25:01 +0100

    Configured to use stardog database

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,9 +32,13 @@ Tripod.configure do |config|
   # config.query_endpoint = 'http://127.0.0.1:3030/tripod-test/sparql'
   # config.data_endpoint = 'http://127.0.0.1:3030/tripod-test/data'
 
-  config.update_endpoint = 'http://127.0.0.1:3002/sparql/raw/update'
-  config.query_endpoint = 'http://127.0.0.1:3002/sparql/raw'
+  #config.update_endpoint = 'http://127.0.0.1:3002/sparql/raw/update'
+  #config.query_endpoint = 'http://127.0.0.1:3002/sparql/raw'
   #config.data_endpoint = 'http://127.0.0.1:3030/tripod-test/data'
+
+  config.update_endpoint = 'http://127.0.0.1:5820/tripod-test/query'
+  config.query_endpoint  = 'http://127.0.0.1:5820/tripod-test/query'
+
 end
 
 # Autoload every model for the test suite that sits in spec/app/models.
