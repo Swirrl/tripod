aa0c372c511f4fa65239fd0f0512cb52053cf1ae
Alex Lynham <acelynham@hotmail.com>
2017-03-15 11:39:34 +0000

    Have a stab at travis.

---
+++ travis/bootstrap_fuseki.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p /home/travis/tdb_data
+mkdir -p /opt/fuseki
+mkdir /opt/fuseki/config
+sudo apt-get update
+sudo apt-get -y install tar wget curl openjdk-7-jre-headless
+wget http://apache.mirror.anlx.net/jena/binaries/jena-fuseki1-1.1.2-distribution.tar.gz -O /opt/fuseki/jena-fuseki-1.1.2.tar.gz
+tar -xvzf /opt/fuseki/jena-fuseki-1.1.2.tar.gz -C /opt/fuseki
+mv ./travis/config_tripod.ttl /opt/fuseki/config/config.ttl
