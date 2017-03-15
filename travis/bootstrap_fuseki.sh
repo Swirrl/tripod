b22eb4d0e896cd98c20e5b78d3c93e855a98bcde
Alex Lynham <acelynham@hotmail.com>
2017-03-15 11:55:09 +0000

    Change fuseki versions and locations to ones that are still available.

--- travis/bootstrap_fuseki.sh
+++ travis/bootstrap_fuseki.sh
@@ -5,6 +5,6 @@ mkdir -p /opt/fuseki
 mkdir /opt/fuseki/config
 sudo apt-get update
 sudo apt-get -y install tar wget curl openjdk-7-jre-headless
-wget http://apache.mirror.anlx.net/jena/binaries/jena-fuseki1-1.1.2-distribution.tar.gz -O /opt/fuseki/jena-fuseki-1.1.2.tar.gz
-tar -xvzf /opt/fuseki/jena-fuseki-1.1.2.tar.gz -C /opt/fuseki
+wget http://apache.mirror.anlx.net/jena/binaries/jena-fuseki1-1.5.0-distribution.tar.gz -O /opt/fuseki/jena-fuseki-1.5.0.tar.gz
+tar -xvzf /opt/fuseki/jena-fuseki-1.5.0.tar.gz -C /opt/fuseki
 mv ./travis/config_tripod.ttl /opt/fuseki/config/config.ttl
--- travis/run_fuseki.sh
+++ travis/run_fuseki.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-FUSEKI_HOME=/opt/fuseki/jena-fuseki1-1.1.2
-cd /opt/fuseki/jena-fuseki1-1.1.2
+FUSEKI_HOME=/opt/fuseki/jena-fuseki1-1.5.0
+cd /opt/fuseki/jena-fuseki1-1.5.0
 env JVM_ARGS=-Xmx4096M ./fuseki-server --config="/opt/fuseki/config/config.ttl"
