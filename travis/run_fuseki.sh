cf91c0a323108c6febf728425fa6c400dfaf0fd8
Alex Lynham <acelynham@hotmail.com>
2017-03-15 12:24:06 +0000

    Make sure fuseki doesn't block in foreground.

--- travis/run_fuseki.sh
+++ travis/run_fuseki.sh
@@ -2,4 +2,4 @@
 
 FUSEKI_HOME=/opt/fuseki/jena-fuseki1-1.5.0
 cd /opt/fuseki/jena-fuseki1-1.5.0
-env JAVA_HOME=/usr/lib/jvm/java-8-oracle/bin/java JVM_ARGS=-Xmx4096M ./fuseki-server --config="/opt/fuseki/config/config.ttl"
+env JAVA_HOME=/usr/lib/jvm/java-8-oracle/bin/java JVM_ARGS=-Xmx4096M ./fuseki-server --config="/opt/fuseki/config/config.ttl" &
